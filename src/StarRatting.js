5e841fe7c9d74beaee480317cda0468f85c1a9fa
swasty jay <amekpoagbedaniel@gmail.com>
2024-12-19 17:09:32 +0000

    creating Reusable Star

--- src/StarRatting.js
+++ src/StarRatting.js
@@ -1,12 +1,27 @@
-export default function StarRatting() {
+const containerStyle = {
+  display: "flex",
+  alignItems: "center",
+  gap: "16px  ",
+};
+
+const Starstyle = {
+  display: " flex",
+  gap: "4px",
+};
+
+const TextStyle = {
+  lineHeight: "1",
+  margin: "0",
+};
+export default function StarRatting({ maxRating = 5 }) {
   return (
-    <div>
-      <div>
-        {Array.from({ length: 5 }, (__, i) => (
+    <div style={containerStyle}>
+      <div style={Starstyle}>
+        {Array.from({ length: maxRating }, (__, i) => (
           <span>⭐{i + 1}</span>
         ))}
       </div>
-      <p>10</p>
+      <p style={TextStyle}>10</p>
     </div>
   );
 }
