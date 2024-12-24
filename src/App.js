bbf9a7bb505244517820fd8b3a7f41069fc2a3e0
swasty jay <amekpoagbedaniel@gmail.com>
2024-12-24 06:55:15 +0000

    fetching movie data

--- src/App.js
+++ src/App.js
@@ -58,7 +58,7 @@ export default function App() {
   useEffect(function () {
     fetch(`http://www.omdbapi.com/?apikey=${KEY}&s=interstellar`)
       .then((res) => res.json())
-      .then((data) => console.log(data)); //.then(data=>console.log(data.Search))
+      .then((data) => setMovies(data.Search)); //.then(data=>console.log(data.Search))
   }, []);
 
   return (
